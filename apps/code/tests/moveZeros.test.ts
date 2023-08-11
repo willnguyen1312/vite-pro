dcbc1b035a2710e22310c17f6b29bd526758541a
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-08-11 15:15:09 -0300

    😇

---
+++ apps/code/tests/moveZeros.test.ts
@@ -0,0 +1,35 @@
+export function moveZeros(input: number[]): number[] {
+    const result: number[] = []
+    let countZeros = 0
+
+    for (const num of input) {
+        if (num === 0) {
+            countZeros++
+        } else {
+            result.push(num)
+        }
+    }
+
+    while (countZeros > 0) {
+        result.push(0)
+        countZeros--
+    }
+
+    return result
+}
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+describe("moveZeros", () => {
+    it("should work as expected", () => {
+        const input = [3, 55, 96, -3, 0, 35, 0, 0, 555, 2]
+        // ...........................2.................0
+        // ..................................555....0...0]
+        // Time complexity: O(n)
+        // Space complexity: O(1)
+        const expected = [3, 55, 96, -3, 35, 555, 2, 0, 0, 0]
+
+        const output = moveZeros(input)
+        expect(output).toEqual(expected)
+    })
+})
--- apps/react/vite.config.ts
+++ apps/react/vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react"
-import UnoCSS from 'unocss/vite'
+import UnoCSS from "unocss/vite"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
