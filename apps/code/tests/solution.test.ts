3bf237174a2bb1936966b51a544e57a6bbd50964
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-08-14 15:13:13 -0300

    🧹

--- apps/code/tests/solution.test.ts
+++ apps/code/tests/solution.test.ts
@@ -1,8 +1,6 @@
 export function solution(input) {
     return input
 }
-// Time complexity: O(n)
-// Space complexity: O(n)
 
 describe("solution", () => {
     it("should work as expected", () => {
