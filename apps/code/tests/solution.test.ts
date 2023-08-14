85e1012981f63727528629682eda633d99c871a9
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-08-14 11:40:04 -0300

    Add solution solution xD

---
+++ apps/code/tests/solution.test.ts
@@ -0,0 +1,15 @@
+export function solution(input) {
+    return input
+}
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+describe("solution", () => {
+    it("should work as expected", () => {
+        const input = true
+        const expected = true
+
+        const output = solution(input)
+        expect(output).toEqual(expected)
+    })
+})
