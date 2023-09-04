ed92a78b4290e62fb1b9ebfd21e0744e8bbc94f5
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-09-04 15:53:29 -0300

    💞

--- apps/react/src/App.tsx
+++ apps/react/src/App.tsx
@@ -1,5 +1,32 @@
+import "./styles.css"
+
+const boxes: any[][] = new Array(15).fill(new Array(8).fill(null))
+
 function App() {
-    return <h1 className="text-lg text-violet">Vite + React</h1>
+    return (
+        <table
+            style={{
+                width: 800,
+                height: 500,
+                position: "relative",
+            }}
+        >
+            <tbody>
+                {boxes.map((row, i) => {
+                    return (
+                        <tr key={i}>
+                            {row.map((_, j) => (
+                                <td key={j}>
+                                    {Math.abs(i - 14)}:
+                                    {Math.floor(8 / 2 - Math.abs(j - 7))}
+                                </td>
+                            ))}
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
 }
 
 export default App
