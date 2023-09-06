17e69b9328be96173cf787d2104f06d421ee58fe
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-09-06 18:00:20 -0300

    🧹

---
+++ apps/code/tests/sample6.test.ts
@@ -0,0 +1,17 @@
+export const solution6 = (n: number) => {
+    let result = 4
+
+    return result
+}
+
+describe("solution6", () => {
+    it("should work", () => {
+        const input = 3
+
+        const output = solution6(input)
+
+        const expected = 4
+
+        expect(output).toEqual(expected)
+    })
+})
---
+++ apps/code/tests/sample7.test.ts
@@ -0,0 +1,17 @@
+export const solution7 = (n: number) => {
+    let result = 4
+
+    return result
+}
+
+describe("solution7", () => {
+    it("should work", () => {
+        const input = 3
+
+        const output = solution7(input)
+
+        const expected = 4
+
+        expect(output).toEqual(expected)
+    })
+})
