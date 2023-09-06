0aa66087d98fdf748f8ef7d70dadec0bebe947b8
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-09-06 18:08:40 -0300

    xD

--- apps/code/tests/sample6.test.ts
+++ apps/code/tests/sample6.test.ts
@@ -1,16 +1,53 @@
-export const solution6 = (n: number) => {
-    let result = 4
+export const solution = (numbers: number[]) => {
+    let largestSum = 0;
+    while (numbers.length > 0) {
+        let previousNumber = numbers.shift()
+        if (previousNumber < 0) {
+            return -1;
+        }
 
-    return result
+        let sum = previousNumber;
+
+        while (numbers.length) {
+            let currentNumber = numbers.shift()
+            if (currentNumber < 0) {
+                return -1;
+            }
+
+            if (currentNumber === previousNumber - 1) {
+                sum += currentNumber;
+                previousNumber = currentNumber;
+            } else {
+                numbers.unshift(currentNumber);
+                break;
+            }
+        }
+
+        largestSum = Math.max(largestSum, sum);
+    }
+
+    return largestSum;
 }
+
+// Given an array of numbers, find the largest sum of consecutive decreasing positive integers.
+
+// Notes:
+
+// Duplicate values should be ignored.
+// If any value in the sequence is not a positive integer, return -1.
+// For example:
+
+// Input: [9, 5, 12, 6, 5, 4, 1]
+
+// Output: 15 (added 6 + 5 + 4)
 
 describe("solution6", () => {
     it("should work", () => {
-        const input = 3
+        const input = [9, 5, 12, 6, 5, 4, 1]
 
-        const output = solution6(input)
+        const output = solution(input)
 
-        const expected = 4
+        const expected = 15
 
         expect(output).toEqual(expected)
     })
